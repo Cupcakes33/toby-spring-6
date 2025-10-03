b74f36cc4208e6894e6aa20e217272e90c3c1038
Cupcakes33 <rute930627@gmail.com>
2025-10-03 20:46:21 +0900

    feat: 환율 API 호출 기능 구현
    - open.er-api.com API를 통한 환율 정보 조회 기능 추가
    - HttpURLConnection을 사용한 HTTP 요청 구현
    - JSON 처리를 위한 spring-boot-starter-json 의존성 추가
    - IOException 예외 처리 추가

--- src/main/java/org/app/tobyspring/PaymentService.java
+++ src/main/java/org/app/tobyspring/PaymentService.java
@@ -1,17 +1,32 @@
 package org.app.tobyspring;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 public class PaymentService {
-    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
         // 환율 가져오기
+        // https://open.er-api.com/v6/latest/USD
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            String collect = bufferedReader.lines().collect(Collectors.joining());
+            System.out.println(collect);
+        }
+
         // 금액 계산
         // 유효 시간 계산
         return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
     };
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         PaymentService paymentService = new PaymentService();
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
