9b878f81bc8fc081e4c5cb75c172cf6b25425b18
Cupcakes33 <rute930627@gmail.com>
2025-10-08 22:19:43 +0900

    refactor: Spring Container를 사용한 Bean 관리로 전환
    - ObjectFactory에 @Configuration, @Bean 어노테이션 추가
    - Client에서 AnnotationConfigApplicationContext를 통해 Bean 조회
    - Spring IoC 컨테이너 기반 의존성 관리 적용

--- src/main/java/org/app/tobyspring/Client.java
+++ src/main/java/org/app/tobyspring/Client.java
@@ -1,12 +1,15 @@
 package org.app.tobyspring;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        ObjectFactory factory = new ObjectFactory();
-        PaymentService paymentService = factory.paymentService();
+        BeanFactory factory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = factory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(52.7));
         System.out.println(payment);
--- src/main/java/org/app/tobyspring/ObjectFactory.java
+++ src/main/java/org/app/tobyspring/ObjectFactory.java
@@ -1,10 +1,16 @@
 package org.app.tobyspring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
+    @Bean
     public ExRateProvider exRateProvider() {
         return new WebApiExRateProvider();
     }
