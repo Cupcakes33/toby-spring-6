1f66de9f42d07a6c1c03a0713815db8b854ca7e2
Cupcakes33 <rute930627@gmail.com>
2025-10-09 17:37:46 +0900

    feat: 시간 기반 캐시 만료 기능 추가
    - CachedExRateProvider에 cachedAt 필드 추가
    - 30분 경과 시 캐시 만료 및 재조회 로직 구현

--- src/main/java/org/app/tobyspring/CachedExRateProvider.java
+++ src/main/java/org/app/tobyspring/CachedExRateProvider.java
@@ -2,10 +2,13 @@ package org.app.tobyspring;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public class CachedExRateProvider implements ExRateProvider {
     private final ExRateProvider target;
     private BigDecimal cachedExRate;
+    private LocalDateTime cachedAt;
+
 
     public CachedExRateProvider(ExRateProvider target) {
         this.target = target;
@@ -13,8 +16,9 @@ public class CachedExRateProvider implements ExRateProvider {
 
     @Override
     public BigDecimal getExRate(String currency) throws IOException {
-        if(cachedExRate == null) {
+        if(cachedExRate == null || cachedAt == null || cachedAt.isBefore(LocalDateTime.now().minusMinutes(30))) {
             cachedExRate = this.target.getExRate(currency);
+            cachedAt = LocalDateTime.now();
             System.out.println("CACHED");
         }
         return cachedExRate;
