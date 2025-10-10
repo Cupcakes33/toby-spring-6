7dba98aacd7b26af3f98357e94654f6bcb04b4f9
Cupcakes33 <rute930627@gmail.com>
2025-10-10 23:07:59 +0900

    test: @Autowired 적용 및 환율 변경 테스트 추가
    - Spring 테스트 컨텍스트에서 직접 의존성 주입
    - ExRateProviderStub을 활용한 환율 변경 시나리오 검증
    - BeanFactory 수동 생성 코드 제거

--- src/test/java/org/app/tobyspring/payment/SpringPaymentServiceTest.java
+++ src/test/java/org/app/tobyspring/payment/SpringPaymentServiceTest.java
@@ -4,8 +4,12 @@ package org.app.tobyspring.payment;
 import org.app.tobyspring.TestObjectFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -13,16 +17,22 @@ import java.math.BigDecimal;
 import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestObjectFactory.class)
 class SpringPaymentServiceTest {
+    @Autowired PaymentService paymentService;
+    @Autowired ExRateProviderStub exRateProviderStub;
 
     @Test
     @DisplayName("prepare 메서드는 요구사항 3가지를 충족한다.")
     void prepare() throws IOException {
-        BeanFactory factory = new AnnotationConfigApplicationContext(TestObjectFactory.class);
-        PaymentService paymentService = factory.getBean(PaymentService.class);
         Payment payment = paymentService.prepare(1L, "USD", new BigDecimal(100));
-
         assertPaymentAmount(payment, valueOf(100), valueOf(10000));
+
+        exRateProviderStub.setExRate(valueOf(500));
+        Payment payment2 = paymentService.prepare(1L, "USD", new BigDecimal(100));
+        assertPaymentAmount(payment2, valueOf(500), valueOf(50000));
+
     }
     private static void assertPaymentAmount(Payment payment, BigDecimal exRate, BigDecimal convertedExRate) throws IOException{
         assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
